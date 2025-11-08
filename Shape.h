5e67ea10520b7247efe8b6d440ccaf86b24bb286
oreste <muhirwa.g.oreste@gmail.com>
2025-11-08 03:01:48 +0100

    Refactor `Shape` class: add position and color management, improve geometry handling with new data members, and enhance OpenGL setup and drawing methods.

--- Shape.h
+++ Shape.h
@@ -2,29 +2,52 @@
 // Created by muhirwa gabo Oreste on 08/11/2025.
 //
 
+
 #ifndef ARCHIVIZ_SHAPE_H
 #define ARCHIVIZ_SHAPE_H
-#pragma once
-#include <QOpenGLFunctions>
-#include <QOpenGLShaderProgram>
+
+#include <QVector3D>
 #include <QOpenGLBuffer>
 #include <QOpenGLVertexArrayObject>
-#include <QVector3D>
+#include <QVector>
+#include <QOpenGLFunctions>
 
-class Shape : protected QOpenGLFunctions {
+class QOpenGLShaderProgram;
+
+class Shape : protected QOpenGLFunctions
+{
 public:
-    Shape() = default;
+    Shape(const QVector3D &position = QVector3D(0,0,0));
     virtual ~Shape();
 
-    virtual void init();
-    virtual void draw(QOpenGLShaderProgram *program, const QMatrix4x4 &matrix);
+    // Pure virtual function: Must be implemented by derived classes
+    virtual void generateGeometry() = 0;
+
+    // Common OpenGL setup function
+    void init();
+
+    // Common drawing function
+    void drawGeometry();
+
+    // Common properties
+    void setPosition(const QVector3D &p) { m_position = p; }
+    QVector3D getPosition() const { return m_position; }
+    void setColor(const QVector4D &c) { m_color = c; }
+    QVector4D getColor() const { return m_color; }
 
 protected:
+    // Common data members
+    QVector3D m_position;
+    QVector4D m_color = QVector4D(1.0f, 1.0f, 1.0f, 1.0f); // default white
+
     QOpenGLVertexArrayObject m_vao;
-    QOpenGLBuffer m_vbo{QOpenGLBuffer::VertexBuffer};
-    QOpenGLBuffer m_ebo{QOpenGLBuffer::IndexBuffer};
-    QVector<QVector3D> m_vertices;
-    QVector<unsigned int> m_indices;
+    QOpenGLBuffer m_vbo { QOpenGLBuffer::VertexBuffer };
+    QOpenGLBuffer m_ebo { QOpenGLBuffer::IndexBuffer };
+
+    // FIX: Using QVector3D directly is safer for geometry generation
+    QVector<QVector3D> m_geometryVertices; // Geometry data container (temporary)
+    QVector<float> m_vertices;             // Final interleaved buffer data
+    QVector<GLuint> m_indices;             // Index data
 };
 
-#endif //ARCHIVIZ_SHAPE_H
+#endif // ARCHIVIZ_SHAPE_H
