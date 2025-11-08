06b6620fdb6edf636be643c129d97c979276034e
oreste <muhirwa.g.oreste@gmail.com>
2025-11-08 03:01:01 +0100

    Refactor `Cube` to inherit from `Shape` and override geometry generation. Remove redundant OpenGL methods and simplify class structure.

--- Cube.h
+++ Cube.h
@@ -6,34 +6,26 @@
 #include <QOpenGLVertexArrayObject>
 #include <QVector>
 #include <QOpenGLFunctions>
+#include "Shape.h"
 
 class QOpenGLShaderProgram;
 
-class Cube : protected QOpenGLFunctions
+class Cube : public Shape
 {
 public:
     Cube(const QVector3D &position = QVector3D(0,0,0), float size = 5.0f);
-    ~Cube();
+    ~Cube() override; // Add override
 
-    void init();                 // create VAO/VBO/EBO (call with current context)
-    void draw(QOpenGLShaderProgram *program); // draw using currently bound shader program
+    void generateGeometry() override;
 
     void setPosition(const QVector3D &p) { m_position = p; }
     QVector3D getPosition() const { return m_position; }
 
 private:
-    void generateGeometry();
 
-private:
-    QVector3D m_position;
     float m_size;
 
-    QOpenGLVertexArrayObject m_vao;
-    QOpenGLBuffer m_vbo { QOpenGLBuffer::VertexBuffer };
-    QOpenGLBuffer m_ebo { QOpenGLBuffer::IndexBuffer };
 
-    QVector<float> m_vertices;      // interleaved x,y,z
-    QVector<GLuint> m_indices;
 };
 
 #endif // ARCHIVIZ_CUBE_H
